28b70b1ff96ed99b54754e00cf8029e79c61357f
adurangba <isaacabioye5@gmail.com>
2023-05-09 17:25:30 +0100

    admin portal

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName:'Foldable mini Drone',
+        productNumber: '21397',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName:'Nig Reality Drone',
+        productNumber: '19343',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName:'Ruko fu Drone',
+        productNumber: '49347',
+        paymentStatus: 'Refund',
+        shipping: 'Declined',
+    },
+    {
+        productName:'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName:'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName:'DJI Air 2s',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName:'Ruko f11 Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+]
